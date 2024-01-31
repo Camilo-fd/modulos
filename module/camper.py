5731a0ba5075fe2d07686c549b45d1a996d1bedd
Camilo-fd <156461877+Camilo-fd@users.noreply.github.com>
2024-01-31 13:15:24 -0500

    avance en el codigo

--- module/camper.py
+++ module/camper.py
@@ -17,16 +17,118 @@ def save():
         f.close()
     return "Succesfully Camper"
 
-
 def edit():
-    return "Edit to trainer"
+    bandera=True
+    while (bandera):
+        system("clear")
+        print("""
+        ***********************
+        * Acualizacion Camper *
+        ***********************
+        """)
+        codigo = int(input("Ingrese el codigo del camper que deseas actualizar: "))
+        print(f"""
+    ________________________
+    Codigo: {codigo}
+    Nombre: {camper[codigo].get('Nombre')}
+    Apellido: {camper[codigo].get('Apellido')}
+    Edad: {camper[codigo].get('Edad')}
+    Genero: {camper[codigo].get('Genero')}
+    ________________________
+        """)
+        print("¿Este es el camper que deseas actualizar?")
+        print("1. Si")
+        print("2. No")
+        print("3. Salir")
+        opc = int(input())
+        if(opc == 1):
+            info = {
+                "Nombre": input("Ingrese el nombre del camper\n"),
+                "Apellido": input("Ingrese el apellido del camper\n"),
+                "Edad": int(input("Ingrese la edad del camper\n")),
+                "Genero": input("Elija su genero:\n\t"+"\t".join([f"{generos.index(i)+1}. {i}\n" for i in sorted(generos)]))
+            }
+            camper[codigo] = info
+            with open("module/storage/camper.json", "w") as f:
+                data = json.dumps(camper, indent=4)
+                f.write(data)
+                f.close()
+            bandera = False
+        elif(opc == 3):
+            bandera = False
+            system("clear")
+    return "Edit to camper"
 
 def search():
-    print(camper)
-    return "The trainer is avaliable"
+    system("clear")
+    print(f"""
+    *******************
+    *  Lista Campers  *
+    *******************
+    """)
+    for i,val in enumerate(camper):
+        print(f"""
+    ____________________________
+    Codigo: {i}
+    Nombre: {val.get('Nombre')}
+    Apellido: {val.get('Apellido')}
+    Edad: {val.get('Edad')}
+    Genero: {val.get('Genero')}
+    ____________________________
+        """)
+    return "The camper is avaliable"
 
 def delete():
-    return "Trainer deleted"
+    bandera = True
+    while(bandera):
+        system("clear")
+        print("""
+        ***************************
+        * Eliminacion Camper  *
+        ***************************
+        """)
+        codigo = int(input("Ingrese el codigo del camper que deseas eliminar: "))
+        print(f"""
+    ________________________
+    Codigo: {codigo}
+    Nombre: {camper[codigo].get('Nombre')}
+    Apellido: {camper[codigo].get('Apellido')}
+    Edad: {camper[codigo].get('Edad')}
+    Genero: {camper[codigo].get('Genero')}
+    ________________________
+        """)
+        print("¿Este es el camper que deseas eliminar?")
+        print("1. Si")
+        print("2. No")
+        print("3. Salir")
+        opc = int(input())
+        if(opc == 1):
+            camper.pop(codigo)
+            with open("module/storage/camper.json", "w") as f:
+                data = json.dumps(camper, indent=4)
+                f.write(data)
+                f.close()
+            bandera = False
+        elif(opc == 2):
+            print("________________________")
+            print("Que desea hacer entonces?")
+            print("1. Volver al menu")
+            print("2. Eliminar camper")
+            opc2 = int(input())
+            if (opc2 == 1):
+                system("clear")
+                menu()
+            elif (opc2 == 2):
+                system("clear")
+                camper.pop(codigo)
+                with open("module/storage/camper.json", "w") as f:
+                    data = json.dumps(camper, indent=4)
+                    f.write(data)
+                    f.close()
+                bandera = False
+        elif(opc == 3):
+            bandera = False
+    return "Camper deleted"
 
 def menu():
     bandera = True
@@ -44,6 +146,12 @@ def menu():
             case 2:
                 system("clear")
                 search()
+            case 3:
+                system("clear")
+                edit()
+            case 4:
+                system("clear")
+                delete()
             case 0:
                 system("clear")
                 bandera = False
--- module/trainer.py
+++ module/trainer.py
@@ -18,14 +18,117 @@ def save():
     return "Succesfully Trainer"
 
 def edit():
+    bandera=True
+    while (bandera):
+        system("clear")
+        print("""
+        ***********************
+        * Acualizacion Trainer*
+        ***********************
+        """)
+        codigo = int(input("Ingrese el codigo del trainer que deseas actualizar: "))
+        print(f"""
+    ________________________
+    Codigo: {codigo}
+    Nombre: {trainer[codigo].get('Nombre')}
+    Apellido: {trainer[codigo].get('Apellido')}
+    Edad: {trainer[codigo].get('Edad')}
+    Genero: {trainer[codigo].get('Genero')}
+    ________________________
+        """)
+        print("¿Este es el trainer que deseas actualizar?")
+        print("1. Si")
+        print("2. No")
+        print("3. Salir")
+        opc = int(input())
+        if(opc == 1):
+            info = {
+                "Nombre": input("Ingrese el nombre del trainer\n"),
+                "Apellido": input("Ingrese el apellido del trainer\n"),
+                "Edad": int(input("Ingrese la edad del trainer\n")),
+                "Genero": input("Elija su genero:\n\t"+"\t".join([f"{generos.index(i)+1}. {i}\n" for i in sorted(generos)]))
+            }
+            trainer[codigo] = info
+            with open("module/storage/trainer.json", "w") as f:
+                data = json.dumps(trainer, indent=4)
+                f.write(data)
+                f.close()
+            bandera = False
+        elif(opc == 3):
+            bandera = False
+            system("clear")
     return "Edit to trainer"
 
 def search():
-    print(trainer)
+    system("clear")
+    print(f"""
+    *******************
+    *  Lista Trainer  *
+    *******************
+    """)
+    for i,val in enumerate(trainer):
+        print(f"""
+    ____________________________
+    Codigo: {i}
+    Nombre: {val.get('Nombre')}
+    Apellido: {val.get('Apellido')}
+    Edad: {val.get('Edad')}
+    Genero: {val.get('Genero')}
+    ____________________________
+        """)
     return "The trainer is avaliable"
 
 def delete():
-    return "Trainer deleted"
+    bandera = True
+    while(bandera):
+        system("clear")
+        print("""
+        ***************************
+        * Eliminacion Trainer  *
+        ***************************
+        """)
+        codigo = int(input("Ingrese el codigo del trainer que deseas eliminar: "))
+        print(f"""
+    ________________________
+    Codigo: {codigo}
+    Nombre: {trainer[codigo].get('Nombre')}
+    Apellido: {trainer[codigo].get('Apellido')}
+    Edad: {trainer[codigo].get('Edad')}
+    Genero: {trainer[codigo].get('Genero')}
+    ________________________
+        """)
+        print("¿Este es el trainer que deseas eliminar?")
+        print("1. Si")
+        print("2. No")
+        print("3. Salir")
+        opc = int(input())
+        if(opc == 1):
+            trainer.pop(codigo)
+            with open("module/storage/trainer.json", "w") as f:
+                data = json.dumps(trainer, indent=4)
+                f.write(data)
+                f.close()
+            bandera = False
+        elif(opc == 2):
+            print("_________________________")
+            print("Que desea hacer entonces?")
+            print("1. Volver al menu")
+            print("2. Eliminar camper")
+            opc2 = int(input())
+            if (opc2 == 1):
+                system("clear")
+                menu()
+            elif (opc2 == 2):
+                system("clear")
+                trainer.pop(codigo)
+                with open("module/storage/camper.json", "w") as f:
+                    data = json.dumps(trainer, indent=4)
+                    f.write(data)
+                    f.close()
+                bandera = False
+        elif(opc == 3):
+            bandera = False
+    return "trainer deleted"
 
 def menu():
     bandera = True
@@ -43,6 +146,12 @@ def menu():
             case 2:
                 system("clear")
                 search()
+            case 3:
+                system("clear")
+                edit()
+            case 4:
+                system("clear")
+                delete()
             case 0:
                 system("clear")
                 bandera = False
